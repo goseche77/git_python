2013ced8c1e338b24b1de054521778fef9ae784f
Gustavo Oseche <goseche@GEP.local>
2024-06-19 21:57:22 -0400

    add new info

---
+++ ciclos.py
@@ -0,0 +1,15 @@
+# ciclos o bucles
+
+# ciclos principales en python - fot - while
+
+# Ciclo for: Se utiliza cuando se conoce de antemano cuantas veces se va a repetir un bloque de codigo. Se va a iterar sobre una secuencia de elementos, como una lista, tupla, rango.
+
+# Ciclo while: Se utiliza cuando se repite un bloque de codigo mientras se cumple una condicion booleana. El bloque de codigo se ejecuta hasta que la condicion que se esta evaluando sea falsa.
+
+# Bucle simple
+i = 1
+# print(i <= 5)
+while i <= 100:
+    if(i % 2 == 0):
+        print(i)
+    i += 1
