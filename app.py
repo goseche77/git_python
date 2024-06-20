4be13848b6333d3f5af022994aacb2558f588c46
Gustavo Oseche <goseche@GEP.local>
2024-06-19 21:16:53 -0400

    develop python

---
+++ app.py
@@ -0,0 +1,36 @@
+#print ("Hola Mundo")
+
+#Inventario de Equipos TI
+
+equipos = ["MODELO", "TIPO", "MEMORIA RAM", "TIPO DE DISCO", "CPU", "SERIAL", "USUARIO", "CARGO", "SO", "AREA"]
+
+print(equipos)
+
+# recorrer elementos de la lista
+"""
+for item in equipos:
+    print(item) """
+
+# TODO CRUD = CREATE , READ, UPDATE, DELETE
+#CREATE / #READ
+#equipos.append("OBSERVACIONES")
+#equipos.insert(9, "DETALLES")
+#print(equipos)
+#print(equipos[3])
+
+#UPDATE
+#(equipos[3])="DISK TYPE"
+#print(equipos)
+
+#DELETE
+#equipos.pop(9)
+#equipos.remove("OBSERVACIONES")
+#equipos.remove("DETALLES")
+#print(equipos)
+
+#equipos.sort()
+equipos.reverse()
+print(equipos)
+
+#Trabajar con Listas
+
---
+++ booleano.py
@@ -0,0 +1,33 @@
+# Operaciones Booleanas simples
+
+# a = True
+# b = False
+
+# print (a and b)
+# print (a or b)
+# print (not a)
+
+# comparaciones de valores #
+
+# x = 5
+# y = 10
+
+# print(x > y)
+# print(x == y)
+# print(x != y)
+
+# Operaciones booleanas con variables ##
+
+# c = True
+# d = False
+
+# result = c and (not d)
+# print(result)
+
+# Combinacion de operadores booleanos #
+
+p = True
+q = False
+
+result = (p and q) or (not p) and (not q)
+print(result)
